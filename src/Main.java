298ae575f67f9a5c44b9573154d3033368c54efb
Balkh <ariasuhrab@gmail.com>
2024-07-11 17:45:23 -0400

    ne code added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Hello world");
+        for (int i = 0; i <10; i++) {
+            System.out.println("Batch 19 is great");
+            Thread.sleep(2000);
+
+        }
 
     }
 }
